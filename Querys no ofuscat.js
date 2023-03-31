6f85970fd0f40a127f893d0701af7b2794ccb338
pater <agustinpaternoster@gmail.com>
2023-03-31 10:28:13 +0200

    correciones

--- Querys no ofuscat.js
+++ Querys no ofuscat.js
@@ -1,3 +1,4 @@
+
 //Escriu una consulta per mostrar tots els documents en la col·lecció Restaurants.
 db.restaurants.find({});
 //Escriu una consulta per mostrar el restaurant_id, name, borough i cuisine de tots els documents en la col·lecció Restaurants.
